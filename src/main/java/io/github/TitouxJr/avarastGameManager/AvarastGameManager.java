0cd1ebfab886ed0ebe1627a012ffbf62e484a9da
TitouxJr <111535159+TitouxJr@users.noreply.github.com>
2025-01-05 05:27:01 +0100

    0.0.1

---
+++ src/main/java/io/github/TitouxJr/avarastGameManager/AvarastGameManager.java
@@ -0,0 +1,17 @@
+package io.github.TitouxJr.avarastGameManager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AvarastGameManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
